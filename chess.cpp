5bc123ed8e6abbe8a51c081a16ff5ac1baf284fc
Orbyfied <officialglitchout@gmail.com>
2023-02-02 10:11:05 +0100

    init

---
+++ chess.cpp
@@ -0,0 +1,6 @@
+/*
+ * By orbyfied (2023)
+ * License can be found at https://github.com/orbyfiedis/badchessengine
+ */
+
+#include "chess.h"
---
+++ chess.h
@@ -0,0 +1,11 @@
+/*
+ * By orbyfied (2023)
+ * License can be found at https://github.com/orbyfiedis/badchessengine
+ */
+
+#ifndef BADCHESSENGINE_CHESS_H
+#define BADCHESSENGINE_CHESS_H
+
+
+
+#endif //BADCHESSENGINE_CHESS_H
